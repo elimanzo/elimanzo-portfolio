306adbce8f438a6f5897ca962002360ef3a0cb6d
Eli Manzo <96manzo.eli@gmail.com>
2024-04-27 17:34:22 -0700

    feat(footer): adding footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import { Inter } from 'next/font/google';
 
 import { Toaster } from 'react-hot-toast';
 
+import Footer from '@/components/footer';
 import Header from '@/components/header';
 import ActiveSectionContextProvider from '@/context/active-section-context';
 
@@ -28,6 +29,7 @@ export default function RootLayout({
         <ActiveSectionContextProvider>
           <Header />
           {children}
+          <Footer />
           <Toaster position="top-right" />
         </ActiveSectionContextProvider>
       </body>
